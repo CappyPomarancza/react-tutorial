86bb0ee1745528dcaded56f5479ac0b3b92a1342
Kacper Swietlaga <kacperres@gmail.com>
2018-08-03 16:59:32 +0200

    something goes wrong

--- src/index.js
+++ src/index.js
@@ -48,25 +48,7 @@ class Board extends React.Component {
 }
 
 
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
-}
+
 class Game extends React.Component {
     constructor(props) {
         super(props);
@@ -80,7 +62,6 @@ class Game extends React.Component {
     }
     handleClick(i) {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
-        const history = this.state.history;
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -142,5 +123,24 @@ class Game extends React.Component {
 
 
 
-ReactDOM.render(<Board />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById('root'));
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
