d70a67f3b13a507a05ae39ac83d1c1714de2fae7
Kacper Swietlaga <kacperres@gmail.com>
2018-08-03 15:58:56 +0200

    added Game class and give him contorle of Board data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,35 @@ import ReactDOM from 'react-dom'
 import './index.css'
 //import App from './App'
 
+
+
+
+class Game extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null),
+        }],
+        xIsNext: true,
+      };
+    }
+  
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+
 class Board extends React.Component {
     constructor(props) {
         super(props)
