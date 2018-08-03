5abc17732bf82d0eaab1956cdefe255b13f4d9d2
Kacper Swietlaga <kacperres@gmail.com>
2018-08-03 15:29:54 +0200

    change Sqaure class to functional component

--- src/index.js
+++ src/index.js
@@ -50,18 +50,17 @@ class Board extends React.Component {
     }
 }
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button
+            className="square"
+            onClick={() => props.onClick()}
+        >
+            {props.value}
+        </button>
+    );
 }
+
 
 
 ReactDOM.render(<Board />, document.getElementById('root'));
