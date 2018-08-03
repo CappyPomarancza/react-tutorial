501927cf33ae9c30f25bde0f0f3ca7249537fc23
Kacper Swietlaga <kacperres@gmail.com>
2018-08-03 07:49:46 +0200

    maked interactive component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom'
 import './index.css'
 //import App from './App'
 
+class Board extends React.Component {
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    renderSquare(i) {
+        return <Square value={i} />
+    }
+}
+
+class Square extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: null,
+        }
+    }
+    render() {
+        return (
+            <button className="square" onClick={() => { alert('click'); }}>
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+
+ReactDOM.render(<Board />, document.getElementById('root'));
 
